0322e3d7fc0661258e25d84e6edbb94b47a79447
Fauzan <fauzanlaode@yahoo.co.id>
2020-12-26 18:30:13 +0700

    try to fix attributes

--- src/main/java/com/dicoding/covidbot/model/Attributes.java
+++ src/main/java/com/dicoding/covidbot/model/Attributes.java
@@ -1,12 +1,16 @@
 package com.dicoding.covidbot.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Attributes implements Serializable {
     private String FID;
     private String Kode_Provi;
